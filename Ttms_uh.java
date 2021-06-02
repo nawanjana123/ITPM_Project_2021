f714fca84bca85e1584e4f0bb19a3a287badb1e1
Rekhanee <30996376+Rekhanee@users.noreply.github.com>
2021-06-02 13:27:19 +0530

    Add files via upload

---
+++ Ttms_uh.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ttms_uh;
+
+/**
+ *
+ * @author Nethmi Dasanayaka
+ */
+public class Ttms_uh {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+    
+}
